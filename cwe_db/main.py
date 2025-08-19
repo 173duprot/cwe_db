f550d51226492566813d5d203c93ff2a38c8d356
lizard_demon <iamjustafreak41@gmail.com>
2025-08-19 19:24:58 -0400

    Cleanup

--- cwe_db/main.py
+++ cwe_db/main.py
@@ -16,10 +16,10 @@ def record(db_path, manifest_path, root_path, min_lines=6):
     sql = sqlite3.connect(db_path);
     sql.cursor().execute("CREATE TABLE IF NOT EXISTS funcs (cve TEXT,file TEXT,start INT,end INT,vuln TEXT,code TEXT,PRIMARY KEY(cve, file))")
 
-    manifest = {f.get('path'): [(n.get('name'), int(n.get('line')))
-                    for n in f.findall('flaw') if n.get('line')]
+    manifest = {f.get('path'): (flaws[0].get('name'), [int(n.get('line'))
+                    for n in flaws if n.get('line')])
                 for f in ET.parse(manifest_path).iter('file')
-                if f.findall('flaw')}
+                if (flaws:=f.findall('flaw'))}
 
     # Files
     for f in Path(root_path).rglob("*"):
@@ -35,12 +35,12 @@ def record(db_path, manifest_path, root_path, min_lines=6):
                 code = code[:s] + bytes((ch if ch==10 else 32) for ch in code[s:e]) + code[e:] # (10=\n)(32= )
 
         # Capture
-        flaws = manifest[f.name]; cve = flaws[0][0];
+        cve, lines = manifest[f.name]
         for nodes in QueryCursor(Query(lang, fn)).captures(parser.parse(code).root_node).values():
             for n in nodes:
                 s, e = n.start_point[0]+1, n.end_point[0]+1 # Lines
                 if e - s + 1 < min_lines: continue # Filter
-                vulns = [str(line-s)for _,line in flaws if s<=line<=e] # Extract
+                vulns = [str(line-s) for(line)in(lines) if(s<=line<=e)] # Extract
 
                 # Record
                 sql.cursor().execute("INSERT OR REPLACE INTO funcs VALUES (?,?,?,?,?,?)",
@@ -49,6 +49,3 @@ def record(db_path, manifest_path, root_path, min_lines=6):
     # Save
     sql.commit();
     sql.close()
-
-
-record("ex.db","./C/manifest.xml","./C/testcases/CWE114_Process_Control/")
